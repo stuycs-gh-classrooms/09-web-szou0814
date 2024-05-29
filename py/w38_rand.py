6ae370b4c842ac767f1cc9c4444bfd13ab0b5073
Sarah Zou <144504010+szou0814@users.noreply.github.com>
2024-05-28 20:56:34 -0400

    Update w38_rand.py

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -2,9 +2,19 @@
 print('Content-type: text/html\n')
 
 import random
-n = random.randrange(100)
+ans = random.randrange(100)
+n1 = random.randrange(100)
+n2 = random.randrange(100)
+
 print('Hello World! Let us play a game.')
 print('<br>' * 2)
 print('I am thinking of a number from 1 to 100. Take a guess!')
 print('<br>' * 2)
-print('<b>', n,'</b>', 'is the correct answer! Did you guess it right? Play again!')
+print('<br>', ans,'</b>', 'is the correct answer! Did you guess it right? +2 if you did!')
+print('<br>' * 2)
+print('Here are a list of runner up answers:')
+print('<ul><li>', n1, '</li><li>', n2, '</li></ul>')
+print('<br>')
+print('+1 if you guessed any of them right!')
+print('<br>' * 2)
+print('Let us play again!')
