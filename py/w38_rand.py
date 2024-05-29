b2eabd9360929367799d94f64b61d7e99384f881
Sarah Zou <144504010+szou0814@users.noreply.github.com>
2024-05-28 20:34:00 -0400

    Update w38_rand.py

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 print('Content-type: text/html/n')
 
-print('Hello World!')
+import random
+n = random.randrange(100)
+print('Hello World! Let us play a game.')
+print('''\n''')
+print('I am thinking of a number from 1 to 100. Take a guess!')
+print('=' * 50)
+print(n, 'is the correct answer! Did you guess it right? Play again!')
