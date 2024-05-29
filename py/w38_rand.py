088c6ce73b0cff376e1963486d34a41b5aae051d
Sarah Zou <144504010+szou0814@users.noreply.github.com>
2024-05-28 20:46:53 -0400

    Update w38_rand.py

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -4,6 +4,8 @@ print('Content-type: text/html\n')
 import random
 n = random.randrange(100)
 print('Hello World! Let us play a game.')
+print('''\n''')
 print('I am thinking of a number from 1 to 100. Take a guess!')
+print('''\n''')
 print('=' * 50)
 print(n, 'is the correct answer! Did you guess it right? Play again!')
